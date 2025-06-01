74ee4916e8cf5d2642e66d67cae676ed2ac7486f
Pooja <pooja07@nagarro.com>
2025-06-01 15:53:20 +0530

    added blob images in products

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 products = [
-    {'id': 1, 'name': 'Product 1', 'price': 10.00},
-    {'id': 2, 'name': 'Product 2', 'price': 20.00},
-    {'id': 3, 'name': 'Product 3', 'price': 30.00},
+    {'id': 1, 'name': 'Product 1', 'price': 10.00, 'image_url': 'https://assigmentstaticfilestore.blob.core.windows.net/assignment-images/product1.jpg'},
+    {'id': 2, 'name': 'Product 2', 'price': 20.00, 'image_url': 'https://assigmentstaticfilestore.blob.core.windows.net/assignment-images/product2.jpg'},
+    {'id': 3, 'name': 'Product 3', 'price': 30.00, 'image_url': 'https://assigmentstaticfilestore.blob.core.windows.net/assignment-images/product3.jpg'},
 ]
 
 @app.route('/')
